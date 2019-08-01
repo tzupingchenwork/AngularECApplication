44afe2fb2373de72d38a996ec25e65f8dc635f79
Andy <>
2019-08-01 20:43:21 +0800

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// constant
+import { appPath } from './app-path.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularECApplication';
+  path = appPath;
 }
--- src/app/product-section/product-section-routing.module.ts
+++ src/app/product-section/product-section-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductSectionComponent } from './product-section.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductSectionComponent,
+    children: [
+      {
+        path: ':type',
+        component: ProductListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

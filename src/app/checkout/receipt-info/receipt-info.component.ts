5e8f1eae9e663172379adb02e701012ca583cd4b
Andy <>
2019-08-02 14:20:13 +0800

    checkout page

--- src/app/checkout/receipt-info/receipt-info.component.ts
+++ src/app/checkout/receipt-info/receipt-info.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 // Constant
 import { appPath } from '../../app-path.const';
+// Enum
+import { SendType } from './send-type.enum';
 @Component({
   selector: 'app-receipt-info',
   templateUrl: './receipt-info.component.html',
   styleUrls: ['./receipt-info.component.css']
 })
 export class ReceiptInfoComponent implements OnInit {
+  // 給 Template 用的路由定義
   path = appPath;
-
+  //  給 Template 用的寄送方式列舉
+  sendType = SendType;
+  // 當前的寄送類型，預設使用 Email
+  selectedType = SendType.EMAIL;
   constructor() { }
 
   ngOnInit() {
   }
 
+  switch(type: number): void {
+    this.selectedType = type;
+  }
+
+  didSelected(type: number): boolean {
+    return this.selectedType === type;
+  }
 }
---
+++ src/app/checkout/receipt-info/send-type.enum.ts
@@ -0,0 +1,9 @@
+export enum SendType {
+
+  // 用 Email 寄送電子發票
+  EMAIL,
+
+  // 寄送實體發票到指定地址
+  ADDRESS
+
+}

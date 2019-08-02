f3d271c09595967a01e88cf48bbc5d41358156b6
Andy <>
2019-08-02 10:19:13 +0800

    共用元件

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ProductItemModule } from '../shared/product-item/product-item.module';
+
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ProductItemModule
   ]
 })
 export class HomeModule { }

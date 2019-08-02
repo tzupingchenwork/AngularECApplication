bbdf8109375cb3ee5fef056706f3532820e189a7
Andy <>
2019-08-02 14:55:28 +0800

    cart bug fix

--- src/app/cart/cart-routing.module.ts
+++ src/app/cart/cart-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart.component';
 
+// Constant
+import { appPath } from '../app-path.const';
 
 const routes: Routes = [
   {
     path: '',
     component: CartComponent
-  }
+  },
+  {
+    path: appPath.checkout,
+    loadChildren: './checkout/checkout.module#CheckoutModule'
+  },
 ];
 
 @NgModule({
